90fba42ebb2376cac92b13158cbcabe10ebaeedf
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:29:06 +0200

    first commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,63 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    /**
+     * @param args
+     */
+    public static void main( String[] args )
+    {
+  
+
+   try {
+        
+         System.out.println("Server Avvito");
+         ServerSocket server =new ServerSocket(3000);
+
+          Socket s =server.accept();
+          System.out.println("Un cliente si è connesso");
+  
+          BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out =  new DataOutputStream(s.getOutputStream());
+
+            String stringaRicevuta = in.readLine();
+            System.out.println("Il client ha inviato " + stringaRicevuta);
+
+            String stringaModificata = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringaModificata + "\n");
+
+            server.close();
+            s.close();
+
+            
+
+       } catch (Exception e) {
+          System.out.println(e.getMessage());
+         }
+
+
+
+    
+
+
+
+}
+
+
+
+
+
+
+    }
+
